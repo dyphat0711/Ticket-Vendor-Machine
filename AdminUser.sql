e9654cb8067407f98d9fa0d8af294afb861160ce
Phat <duyphathust@gmail.com>
2026-04-03 14:32:37 +0700

    New tblUser for Admin Login

---
+++ AdminUser.sql
@@ -0,0 +1,12 @@
+﻿use MetroTicketDB;
+
+CREATE TABLE tblUser (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(50) NOT NULL,
+    Role NVARCHAR(20) DEFAULT 'Admin'
+);
+
+-- Tạo một tài khoản mặc định để test
+INSERT INTO tblUser (Username, Password, Role) 
+VALUES ('admin', '123456', 'Admin');
