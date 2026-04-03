c955c448a8e0a26d47df7e807c07c4e1639d6cb0
nguyen <abc@gmail.com>
2026-04-03 11:34:22 +0700

    add Controller

--- BenThanhMetro/Controllers/HomeController.cs
+++ BenThanhMetro/Controllers/HomeController.cs
@@ -3,28 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BenThanhMetro.Models;
 
 namespace BenThanhMetro.Controllers
 {
     public class HomeController : Controller
     {
+        private MetroTicketDBEntities db = new MetroTicketDBEntities();
+
         public ActionResult Index()
         {
-            return View();
-        }
+            var destinations = db.Destinations.ToList();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(destinations);
         }
     }
 }
